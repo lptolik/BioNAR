0375beba732818473047ca1975cd735de02cf2a3
Anatoly Sorokin <lptolik@gmail.com>
2022-10-20 19:27:56 +0900

    datasets documentation

---
+++ R/datasets.R
@@ -0,0 +1,143 @@
+#' Title for BrProteins_2020.csv
+#'
+#' description for BrProteins_2020.csv
+#'
+#' @name BrProteins_2020.csv
+#' @keywords BrProteins_2020.csv
+#' @docType data
+#' @details
+#'     details for BrProteins_2020.csv
+#'
+NULL
+
+#' Title for CorePSD95Complex.csv
+#'
+#' description for CorePSD95Complex.csv
+#'
+#' @name CorePSD95Complex.csv
+#' @keywords CorePSD95Complex.csv
+#' @docType data
+#' @details
+#'     details for CorePSD95Complex.csv
+#'
+NULL
+
+#' Title for PPI_Presynaptic.csv
+#'
+#' description for PPI_Presynaptic.csv
+#'
+#' @name PPI_Presynaptic.csv
+#' @keywords PPI_Presynaptic.csv
+#' @docType data
+#' @details
+#'     details for PPI_Presynaptic.csv
+#'
+NULL
+
+#' Title for PPI_Presynaptic.gml
+#'
+#' description for PPI_Presynaptic.gml
+#'
+#' @name PPI_Presynaptic.gml
+#' @keywords PPI_Presynaptic.gml
+#' @docType data
+#' @details
+#'     details for PPI_Presynaptic.gml
+#'
+NULL
+
+#' Title for PPI_cluster.gml
+#'
+#' description for PPI_cluster.gml
+#'
+#' @name PPI_cluster.gml
+#' @keywords PPI_cluster.gml
+#' @docType data
+#' @details
+#'     details for PPI_cluster.gml
+#'
+NULL
+
+#' Title for PresynAn.csv
+#'
+#' description for PresynAn.csv
+#'
+#' @name PresynAn.csv
+#' @keywords PresynAn.csv
+#' @docType data
+#' @details
+#'     details for PresynAn.csv
+#'
+NULL
+
+#' Title for SCH_flatfile.csv
+#'
+#' description for SCH_flatfile.csv
+#'
+#' @name SCH_flatfile.csv
+#' @keywords SCH_flatfile.csv
+#' @docType data
+#' @details
+#'     details for SCH_flatfile.csv
+#'
+NULL
+
+#' Title for diseasome.rda
+#'
+#' description for diseasome.rda
+#'
+#' @name diseasome.rda
+#' @keywords diseasome.rda
+#' @docType data
+#' @details
+#'     details for diseasome.rda
+#'
+NULL
+
+#' Title for flatfile.go.BP.csv
+#'
+#' description for flatfile.go.BP.csv
+#'
+#' @name flatfile.go.BP.csv
+#' @keywords flatfile.go.BP.csv
+#' @docType data
+#' @details
+#'     details for flatfile.go.BP.csv
+#'
+NULL
+
+#' Title for flatfile.go.CC.csv
+#'
+#' description for flatfile.go.CC.csv
+#'
+#' @name flatfile.go.CC.csv
+#' @keywords flatfile.go.CC.csv
+#' @docType data
+#' @details
+#'     details for flatfile.go.CC.csv
+#'
+NULL
+
+#' Title for flatfile.go.MF.csv
+#'
+#' description for flatfile.go.MF.csv
+#'
+#' @name flatfile.go.MF.csv
+#' @keywords flatfile.go.MF.csv
+#' @docType data
+#' @details
+#'     details for flatfile.go.MF.csv
+#'
+NULL
+
+#' Title for flatfile_human_gene2HDO.csv
+#'
+#' description for flatfile_human_gene2HDO.csv
+#'
+#' @name flatfile_human_gene2HDO.csv
+#' @keywords flatfile_human_gene2HDO.csv
+#' @docType data
+#' @details
+#'     details for flatfile_human_gene2HDO.csv
+#'
+NULL
